7d094ede99e13276e8278f6a25085d39f0a3364a
goldenbulls <goldenbullssocial@gmail.com>
2026-01-12 18:25:53 +0530

    styles:description change

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,7 @@ import "swiper/css/navigation";
 
 export const metadata = {
   title: "Golden Bulls Learning Academy Private Limited",
-  description: "Golden Bulls Learning Academy Private Limited",
+  description: "Master forex, crypto, and trading strategies with Golden Bulls. Expert-led courses for beginners to advanced traders.",
 };
 
 export default function RootLayout({ children }) {
