364c496eedd26d8628b8c2f3f1e981c18382a3f9
Shreya Rejoice <shreya356.rejoice@gmail.com>
2026-02-13 18:29:02 +0530

    add suspense

--- src/app/(main)/telegram-channel/page.js
+++ src/app/(main)/telegram-channel/page.js
@@ -1,16 +1,18 @@
 import TelegramChannel from '@/modulers/telegramChannel'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 export const metadata = {
-  title: "Telegram Channel - Golden Bulls",
-  description: "Join our Telegram channel for real-time trading signals, market updates, and exclusive content from Golden Bulls.",
-  keywords: "telegram trading, trading signals, market alerts, trading community",
+    title: "Telegram Channel - Golden Bulls",
+    description: "Join our Telegram channel for real-time trading signals, market updates, and exclusive content from Golden Bulls.",
+    keywords: "telegram trading, trading signals, market alerts, trading community",
 };
 
 export default function page() {
     return (
         <div>
-            <TelegramChannel />
+            <Suspense fallback={<div>Loading...</div>}>
+                <TelegramChannel />
+            </Suspense>
         </div>
     )
 }
